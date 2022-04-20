bf83e3d30ff7ed9593497b335f030e55517b8627
Sergio Hinojosa <sergio.hinojosa1@gmail.com>
2022-04-19 20:56:48 -0500

    Make setters for all private variables.

--- maze-game/Assets/Scripts/Cell.cs
+++ maze-game/Assets/Scripts/Cell.cs
@@ -15,16 +15,21 @@ public enum Walls
 
 public class Cell
 {
-    private int x;
-    private int y;
-    private bool visited;
-    private Walls walls;
+    private int _x;
+    private int _y;
+    private bool _visited;
+    private Walls _walls;
 
-    public Cell (int x, int y)
+    public Cell (int xPos, int yPos)
     {
-        this.x = x;
-        this.y = y;
-        this.visited = false;
-        this.walls = Walls.TOP | Walls.RIGHT | Walls.BOTTOM | Walls.LEFT;
+        _x = xPos;
+        _y = yPos;
+        _visited = false;
+        _walls = Walls.TOP | Walls.RIGHT | Walls.BOTTOM | Walls.LEFT;
     }
+
+    public int x => _x;
+    public int y => _y;
+    public bool visited => visited;
+    public Walls walls => _walls;
 }
