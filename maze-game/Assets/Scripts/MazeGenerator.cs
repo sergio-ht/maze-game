898f57b9b3a3f7ec3863317442794a5c57b019cc
Sergio Hinojosa <sergio.hinojosa1@gmail.com>
2022-04-19 20:27:18 -0500

    Create Generate function in MazeGenerator.

--- maze-game/Assets/Scripts/MazeGenerator.cs
+++ maze-game/Assets/Scripts/MazeGenerator.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MazeGenerator : MonoBehaviour
+public class MazeGenerator
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<Cell> Generate(int rows, int cols)
     {
-        
-    }
+        List<Cell> maze = new List<Cell>();
+        for(int y = 0; y < rows; y++)
+        {
+            for (int x = 0; x < cols; x++)
+            {
+                maze.Add(new Cell(x, y));
+            }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        return maze;
     }
 }
