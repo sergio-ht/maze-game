2d532e6003e14e8e33aff99610117d5bdfe1684f
Sergio Hinojosa <sergio.hinojosa1@gmail.com>
2022-04-19 20:36:40 -0500

    Get the maze from MazeGenerator and send it to an empty Draw method.

--- maze-game/Assets/Scripts/MazeRenderer.cs
+++ maze-game/Assets/Scripts/MazeRenderer.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class MazeRenderer : MonoBehaviour
 {
+    [SerializeField]
+    private int _rows;
+
+    [SerializeField]
+    private int _cols;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        var maze = MazeGenerator.Generate(_rows, _cols);
+        Draw(maze);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Draw(List<Cell> maze)
     {
         
     }
