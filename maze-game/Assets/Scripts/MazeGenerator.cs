f1e3a8cf2f5f1a870495cf4f90396e75d287eefc
Sergio Hinojosa <sergio.hinojosa1@gmail.com>
2022-04-20 11:41:02 -0500

    Change the order in which cells are added to the list.

--- maze-game/Assets/Scripts/MazeGenerator.cs
+++ maze-game/Assets/Scripts/MazeGenerator.cs
@@ -7,9 +7,9 @@ public class MazeGenerator
     public static List<Cell> Generate(int rows, int cols)
     {
         List<Cell> maze = new List<Cell>();
-        for(int y = 0; y < rows; y++)
+        for (int x = 0; x < cols; x++)
         {
-            for (int x = 0; x < cols; x++)
+            for (int y = 0; y < rows; y++)
             {
                 maze.Add(new Cell(x, y));
             }
