ea5736c2da4240b0c71395b07a96ae0411fc4c0e
Sergio Hinojosa <sergio.hinojosa1@gmail.com>
2022-04-19 20:07:54 -0500

    Implement cell constructor.
    Cell constructor takes two arguments for position (x, y).
    The cell is marked as not visited.
    It also initializes a flag enum used to represent the walls.

--- maze-game/Assets/Scripts/Cell.cs
+++ maze-game/Assets/Scripts/Cell.cs
@@ -1,18 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cell : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+[Flags]
+public enum Walls
+{
+    TOP = 1,
+    RIGHT = 2,
+    BOTTOM = 4,
+    LEFT = 8,
+}
+
+public class Cell
+{
+    private int x;
+    private int y;
+    private bool visited;
+    private Walls walls;
+
+    public Cell (int x, int y)
     {
-        
+        this.x = x;
+        this.y = y;
+        this.visited = false;
+        this.walls = Walls.TOP | Walls.RIGHT | Walls.BOTTOM | Walls.LEFT;
     }
 }
