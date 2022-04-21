9efff8641fb96173ddfd244cfcb9b92567bf7d1b
Sergio Hinojosa <sergio.hinojosa1@gmail.com>
2022-04-20 19:56:18 -0500

    Implement GetUnvisitedNeighbors to get the unvisited neighboring cells.

--- maze-game/Assets/Scripts/MazeGenerator.cs
+++ maze-game/Assets/Scripts/MazeGenerator.cs
@@ -14,14 +14,32 @@ public class MazeGenerator
                 maze.Add(new Cell(x, y));
             }
         }
-
+        GetUnivisitedNeighbors(maze, new Cell(99, 99), 100, 100);
         return maze;
     }
 
     private static List<Cell> GetUnivisitedNeighbors(List<Cell> maze, Cell cell, int rows, int cols)
     {
         List<Cell> neighbors = new List<Cell>();
+        var steps = new[] { (-1, 0), (1, 0), (0, -1), (0, 1) };
 
+        foreach(var step in steps)
+        {
+            int newX = cell.x + step.Item1;
+            int newY = cell.y + step.Item2;
+            // check bounds
+            if(newX >= 0 && newY >= 0 && newX < cols && newY < rows)
+            {
+                // get index of cell in list (maze)
+                int listIndex = ListIndex(newX, newY, rows);
+
+                // add to neighbors if unvisited
+                if (!maze[listIndex].visited)
+                {
+                    neighbors.Add(maze[listIndex]);
+                }
+            }
+        }
 
         return neighbors;
     }
