01f57ffb28a254d7040cc216c38a93448a1e8e81
Sergio Hinojosa <sergio.hinojosa1@gmail.com>
2022-05-14 09:18:10 -0500

    Add player movement

---
+++ maze-game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5;
+    private float _forwardInput, _horizontalInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _forwardInput = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(_horizontalInput, _forwardInput);
+        transform.Translate(movement * _speed * Time.deltaTime);
+
+    }
+}
