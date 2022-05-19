096948b75a0da29725b6ef847cded38b5ccad90e
Sergio Hinojosa <sergio.hinojosa1@gmail.com>
2022-05-19 18:38:07 -0500

    Add particles when player wins.

--- maze-game/Assets/Scripts/GoalController.cs
+++ maze-game/Assets/Scripts/GoalController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class GoalController : MonoBehaviour
 {
+    [SerializeField]
+    ParticleSystem particles;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
+            particles.Play();
             Debug.Log("You win 123");
         }
     }
