8cb81cde7b6d8fb1bcc80ee0f3a31bb7fbe030a8
Sergio Hinojosa <sergio.hinojosa1@gmail.com>
2022-05-19 19:03:06 -0500

    Show game over panel instead of loading a scene.

--- maze-game/Assets/Scripts/GoalController.cs
+++ maze-game/Assets/Scripts/GoalController.cs
@@ -6,12 +6,21 @@ public class GoalController : MonoBehaviour
 {
     [SerializeField]
     ParticleSystem particles;
+
+    [SerializeField]
+    private GameObject winPanel;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
             particles.Play();
+            Invoke("ShowWinScene", 0.5f);
             Debug.Log("You win 123");
         }
+    }
+
+    private void ShowWinScene()
+    {
+        winPanel.SetActive(true);
     }
 }
