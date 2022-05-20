fef7f45bf19cacc5563beaa5a94b316b763b3091
Sergio Hinojosa <sergio.hinojosa1@gmail.com>
2022-05-19 22:52:09 -0500

    Add sound at the beginning and end of the game.

--- maze-game/Assets/Scripts/GoalController.cs
+++ maze-game/Assets/Scripts/GoalController.cs
@@ -9,11 +9,15 @@ public class GoalController : MonoBehaviour
 
     [SerializeField]
     private GameObject winPanel;
+
+    [SerializeField]
+    private AudioSource winSound;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
             particles.Play();
+            winSound.Play();
             Invoke("ShowWinScene", 0.5f);
             Debug.Log("You win 123");
         }
