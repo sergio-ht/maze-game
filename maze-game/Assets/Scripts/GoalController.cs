e5b73c0dce80cb676d23d426204897c011a8b30f
Sergio Hinojosa <sergio.hinojosa1@gmail.com>
2022-05-25 13:39:38 -0500

    Show points on finish

--- maze-game/Assets/Scripts/GoalController.cs
+++ maze-game/Assets/Scripts/GoalController.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GoalController : MonoBehaviour
 {
@@ -12,12 +14,30 @@ public class GoalController : MonoBehaviour
 
     [SerializeField]
     private AudioSource winSound;
+
+    [SerializeField]
+    private TextMeshProUGUI score;
+
+    private DateTime _startTime;
+
+    void Start()
+    {
+        _startTime = DateTime.Now;
+        Debug.Log("Started on: " + _startTime);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
             particles.Play();
             winSound.Play();
+            
+            TimeSpan endTime = DateTime.Now - _startTime;
+            int totalSeconds = (int)endTime.TotalSeconds;
+
+            Debug.Log("Finished on: " + (int)endTime.TotalSeconds);
+            score.text = totalSeconds.ToString();
+
             Invoke("ShowWinScene", 0.5f);
             Debug.Log("You win 123");
         }
